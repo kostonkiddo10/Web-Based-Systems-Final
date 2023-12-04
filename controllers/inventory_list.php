7a576c5f2a87b93c4f461750416d99131377b6a9
Davian Albarran <dalbarran.dev@gmail.com>
2023-12-03 23:12:55 -0500

    inventory table displays data.

--- controllers/inventory_list.php
+++ controllers/inventory_list.php
@@ -3,5 +3,9 @@ require_once '../models/inventory.php';
 
 $model = new InventoryModel();
 
+$model->getItems();
+
+$items = $model->items;
+
 include_once '../views/inventory_view.php';
 ?>
--- views/inventory_view.php
+++ views/inventory_view.php
@@ -8,7 +8,7 @@
         </tr>
         <?php 
             foreach ($items as $item) {
-                echo '<tr class=available-' . $item['isAvailable'] . '><td>' . $item['item_name'] . '</td><td>' . $item['status'] . '</td><td><img src=' . $item['img_path'] .' /></td></tr>';
+                echo '<tr class=available-' . $item['isAvailable'] . '><td>' . $item['itemName'] . '</td><td>' . $item['status'] . '</td><td><img src=' . $item['img_path'] .' /></td></tr>';
             }
         ?>
     </table>
