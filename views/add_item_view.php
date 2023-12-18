5d26231fc25b284d7da26bd29b3e67b5910fd950
Davian Albarran <dalbarran.dev@gmail.com>
2023-12-18 03:57:25 -0500

    changed some styles. dont know why they wont show right away even after server reboot

--- views/add_item_view.php
+++ views/add_item_view.php
@@ -1,20 +1,25 @@
 <html>
     <head>
         <link rel="stylesheet" href="../styles/global.css">
+        <link rel="stylesheet" href="../styles/add_item_style.css">
     </head>
     <body>
-    <nav class="navbar">
+        <nav class="navbar">
             <ul>
                 <li><a href="./inventory_list.php">Home</a></li>
                 <li><a href="./login.php?action=logout">Logout</a></li>
             </ul>
         </nav>
         <form action="add_item.php" method="post" enctype="multipart/form-data">
-            <label for="imgUpload">*OPTIONAL* Upload a picture of the item:</label>
-            <input type="file" name="imgUpload" id="imgUpload">
+            <div class="input-form">
+                <label class="input-label" for="imgUpload">*OPTIONAL* Upload a picture of the item:</label>
+                <input type="file" name="imgUpload" id="imgUpload">
+            </div>
 
-            <label for="itemName">Item Name</label>
-            <input type="text" name="itemName">
+            <div class="input-form">
+                <label class="input-label" for="itemName">Item Name</label>
+                <input type="text" name="itemName">
+            </div>
 
             <input type="submit" value="Add Item" name="addItem">
         </form>
